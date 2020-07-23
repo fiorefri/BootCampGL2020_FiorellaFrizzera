01a52603ccc04edf9cd2eacc2db392a1a1c330c6
fiorefri <fiorefri@gmail.com>
2020-07-23 05:50:35 -0300

    Ejercicio 3.2

---
+++ Modulo1/EstructurasBasicas/PrimeraVariable.java
@@ -0,0 +1,11 @@
+public class PrimeraVariable {
+    public static void main(String[] args) {
+        String mensaje = "Valor Inicial";
+
+        System.out.println(mensaje);
+
+        mensaje = "Valor Modificado";
+
+        System.out.println(mensaje);
+    }
+}
