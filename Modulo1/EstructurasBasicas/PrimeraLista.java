58d3169c303c8dbc7c56d60a0eaf8a210ec574c1
fiorefri <fiorefri@gmail.com>
2020-07-23 13:39:21 -0300

    Ejercicio 3.3

---
+++ Modulo1/EstructurasBasicas/PrimeraLista.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class PrimeraLista {
+    public static void main(String [] args) {
+        List<String> letters = new ArrayList<String>();
+        letters.add("A");
+        letters.add("B");
+        letters.add("C");
+
+        System.out.println(letters);
+        System.out.println(letters.size());
+    }
+}
