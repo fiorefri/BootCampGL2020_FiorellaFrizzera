213b148e4f716b52592dca2a3819c9c90cf50b6f
fiorefri <fiorefri@gmail.com>
2020-07-24 14:10:32 -0300

    Se arregla el Ejercicio 3.6

--- bootcampgl2020/src/main/java/com/globallogic/app/EstructurasBasicas/EmpleadoDemo.java
+++ bootcampgl2020/src/main/java/com/globallogic/app/EstructurasBasicas/EmpleadoDemo.java
@@ -2,10 +2,10 @@ package com.globallogic.app.EstructurasBasicas;
 
 public class EmpleadoDemo {
     public static void main(String[] args) {
-        Empleado empleado = new Empleado();
+        new Empleado();
 
-        empleado.setSalario(1000);
+        Empleado.setSalario(1000);
 
-        System.out.println(empleado.getNombre() + " tiene un salario promedio de " + empleado.getSalario());
+        System.out.println(Empleado.getNombre() + " tiene un salario promedio de " + Empleado.getSalario());
     }
 }
